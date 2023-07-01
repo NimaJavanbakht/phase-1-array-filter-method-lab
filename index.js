ba180db717e477580cf87d518b65450610cb10d9
Nima Javanbakht <nimajavan230@gmail.com>
2023-06-30 21:04:10 -0700

    completes filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+function findMatching(drivers, personWeAreLookingFor) {
+
+    const newDrivers = drivers.filter(driver => driver.toUpperCase() === personWeAreLookingFor.toUpperCase())
+    return newDrivers;
+}
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+findMatching()
+
+function fuzzyMatch(drivers, firstDriverCharacter) {
+    const newDrivers = drivers.filter(driver => driver.indexOf(firstDriverCharacter) === 0)
+    return newDrivers;
+}
+fuzzyMatch();
+
+function matchName(drivers, driverName) {
+    const newDrivers = drivers.filter(driver => driver["name"] === driverName)
+    return newDrivers;
+    
+}
+    
